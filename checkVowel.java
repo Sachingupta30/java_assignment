5e768db4cf9cf3fe6ea076586ab71f07db2d309c
Sachin Gupta <70026034+Sachingupta30@users.noreply.github.com>
2021-10-19 21:38:44 +0530

    Add files via upload

---
+++ checkVowel.java
@@ -0,0 +1,42 @@
+
+public class checkVowel {
+
+	public static void main(String[] args) {
+		char vowel = 'E';
+		switch (vowel) {
+		case 'a':
+			System.out.print(vowel + " " + "is a vowel");
+			break;
+		case 'A':
+			System.out.print(vowel + " " + "is a vowel");
+			break;
+		case 'e':
+			System.out.print(vowel + " " + "is a vowel");
+			break;
+		case 'E':
+			System.out.print(vowel + " " + "is a vowel");
+			break;
+		case 'i':
+			System.out.print(vowel + " " + "is a vowel");
+			break;
+		case 'I':
+			System.out.print(vowel + " " + "is a vowel");
+			break;
+		case 'o':
+			System.out.print(vowel + " " + "is a vowel");
+			break;
+		case 'O':
+			System.out.print(vowel + " " + "is a vowel");
+			break;
+		case 'u':
+			System.out.print(vowel + " " + "is a vowel");
+			break;
+		case 'U':
+			System.out.print(vowel + " " + "is a vowel");
+			break;
+		default:
+			System.out.print(vowel + " " + "is not a vowel");
+		}
+	}
+
+}
